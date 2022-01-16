1e72a5f8455090f60c0a420bd4dc13120f18ff02
berlshac <berlshac@post.bgu.ac.il>
2022-01-16 02:25:47 -0800

    action 2 longer

--- action2.sh
+++ action2.sh
@@ -1,6 +1,13 @@
 echo "start" 
-sleep 5
-echo "five past"
-sleep 10
-echo "15 past"
-
+sleep 30
+echo "30 past"
+sleep 30
+echo "60 past"
+sleep 30
+echo "90 past"
+sleep 30
+echo "120 past"
+sleep 30
+echo "150 past"
+sleep 30
+echo "180 past"
